52c81a3ca7d12ffdae0ee101de34745ab050603d
Pratik Kumawat <kumawat10@gmail.com>
2023-04-06 16:56:46 +0530

    Initial checkin

---
+++ src/test/java/com/pratik/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratik.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
