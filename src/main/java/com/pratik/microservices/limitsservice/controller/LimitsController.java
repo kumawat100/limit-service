67e7c6cb9eff576d7ed091d48fb87fbbdc7208ae
Pratik Kumawat <kumawat10@gmail.com>
2023-04-06 17:34:55 +0530

    Limits from config

---
+++ src/main/java/com/pratik/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,20 @@
+package com.pratik.microservices.limitsservice.controller;
+
+import com.pratik.microservices.limitsservice.bean.Limits;
+import com.pratik.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMin(), configuration.getMax());
+        //return new Limits(1,10);
+    }
+}
